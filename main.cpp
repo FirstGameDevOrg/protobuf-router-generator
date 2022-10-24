c993c2340b1d2a7b834e45e529013dec4db0fe5d
OhtoAi <zhu.thatboy@outlook.com>
2022-10-24 15:34:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,32 +4,32 @@
 
 namespace ohtoai::protobuf
 {
-	template<typename ...OtherTypes>
-	std::vector<std::string> getMessageNames()
-	{
-		return std::apply(
-			[](auto&&... elems)
-			{
-				std::vector<std::string> result;
-				result.reserve(sizeof...(elems));
-				(result.push_back(std::forward<decltype(elems)>(elems)), ...);
-				return result;
-			}
-			, std::forward<decltype(
-				std::make_tuple(OtherTypes::descriptor()->full_name()...))>(
-					std::make_tuple(OtherTypes::descriptor()->full_name()...)));
-	}
+    template<typename ...OtherTypes>
+    decltype(auto) getMessageNames()
+    {
+        return std::apply(
+            [](auto&&... elems)
+            {
+                std::vector<std::string> result;
+                result.reserve(sizeof...(elems));
+                (result.push_back(std::forward<decltype(elems)>(elems)), ...);
+                return result;
+            }
+            , std::forward<decltype(
+                std::make_tuple(OtherTypes::descriptor()->full_name()...))>(
+                    std::make_tuple(OtherTypes::descriptor()->full_name()...)));
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	auto names = ohtoai::protobuf::getMessageNames<
-		pb::socket::UserLogin
-		, pb::socket::UserRegister
-		, pb::socket::UserInfo
-		, pb::socket::ExternalMessage
-	>();
-	
+    auto names = ohtoai::protobuf::getMessageNames<
+        pb::socket::UserLogin
+        , pb::socket::UserRegister
+        , pb::socket::UserInfo
+        , pb::socket::ExternalMessage
+    >();
+    
     QApplication a(argc, argv);
     ProtobufRouterGenerator w;
     w.show();
